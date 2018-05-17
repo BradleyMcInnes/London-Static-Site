ba8435ef76cdc367f040442352e6755776f85035
BradleyMcInnes <33387856+BradleyMcInnes@users.noreply.github.com>
2018-05-17 06:18:56 +0100

    My first commit

---
+++ London/Scripts/script.js
@@ -0,0 +1,15 @@
+
+//document.getElementById("WestEnd").addEventListener("mouseover", mouseOver);
+//document.getElementById("WestEnd").addEventListener("mouseout", mouseOut);
+
+//document.getElementById("test").onmouseover = function() {
+	//mouseOver()
+//};
+
+//function mouseOver(x) {
+	//x.style.visibility = "visible";
+//}
+
+//function mouseOut(x) {
+	//x.style.display = "none";
+//}
